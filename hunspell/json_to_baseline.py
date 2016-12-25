a50244cb4c21a0fdeb7bff7bca057f614eaef631
graham-laptop <graham_oliver@yahoo.com>
2016-12-25 22:45:07 +0000

    wrote json to baseline

--- hunspell/json_to_baseline.py
+++ hunspell/json_to_baseline.py
@@ -1,35 +1,33 @@
 '''
-Create a test aff file for testing which will be used
-with the file hpk.dic 
-
+Create 2 baseline files
+1) hpk.dic
+2) baseline.aff
 '''
 import config
 import os
 import query_yes_no
 import sys
 
-print()
-
 cf = config.ConfigFile()
-test_dicaff_files_path = (cf.configfile[cf.computername]['test_dicaff_files_path'])
+baseline_files_path = (cf.configfile[cf.computername]['baseline_files_path'])
 
-if sys.argv[1] == "create_test_dic_file":
+if sys.argv[1] == "create_hpk_dic_file":
     file_name = "hpk.dic"
-elif sys.argv[1] == "create_test_aff_file":
-    file_name = "test.aff"
+elif sys.argv[1] == "create_baseline_aff_file":
+    file_name = "baseline.aff"
 else:
     file_name = "oops.oops"
 
-test_dicaff_file_path = test_dicaff_files_path + file_name
+baseline_file_path = baseline_files_path + file_name
 
-if os.path.isfile(test_dicaff_file_path):
+if os.path.isfile(baseline_file_path):
     # file exists, check to see if it should be overridden
     file_exists = True
     overwrite_file = query_yes_no.query_yes_no("OVERWRITE the existing file?")
 else:
     file_exists = False
 
-def create_test_dic_file():
+def create_hpk_dic_file():
 
     if file_exists and not overwrite_file:
         print("Existing file left as is")
@@ -37,19 +35,19 @@ def create_test_dic_file():
 
     if file_exists and overwrite_file:
         # delete existing file
-        os.remove(test_dicaff_file_path)
+        os.remove(baseline_file_path)
 
     # write the file
     import json_processor_hunspell as jph
     all_words_for_hunspell = jph.get_all_words_for_hunspell()
-    with open(test_dicaff_file_path, "a", encoding = 'utf-8') as myfile:
+    with open(baseline_file_path, "a", encoding = 'utf-8') as myfile:
         myfile.write(str(len(all_words_for_hunspell)) + "\n")
         for word in all_words_for_hunspell:
             myfile.write(word + "\n")
     return True
     
 
-def create_test_aff_file():
+def create_baseline_aff_file():
 
     if file_exists and not overwrite_file:
         print("Existing file left as is")
@@ -57,7 +55,7 @@ def create_test_aff_file():
 
     if file_exists and overwrite_file:
         # delete existing file
-        os.remove(test_dicaff_file_path)
+        os.remove(baseline_file_path)
 
 
     #time to create the file
@@ -66,84 +64,15 @@ def create_test_aff_file():
     dt = datetime.now().strftime('%d %b %Y, %H:%M:%S')
     aff_lines = []
 
-    # Part 1
     aff_lines.append('###############################')
-    aff_lines.append('# Māori affix file - Creation #')
+    aff_lines.append('# Māori affix file - BASELINE #')
     aff_lines.append('#### ' + dt + ' ####')
     aff_lines.append('##### pangakupu@gmail.com #####')
     aff_lines.append('###############################')
     aff_lines.append('')
-    aff_lines.append('######################')
-    aff_lines.append('#### part 1 of 3 #####')
-    aff_lines.append('### General Options ##')
-    aff_lines.append('######################')
-    aff_lines.append('')
-    aff_lines.append('##### set #####')
     aff_lines.append('SET UTF-8')
-    aff_lines.append('### end set ###')
-    aff_lines.append('')
-
-    # Part 2
-    aff_lines.append('##############################')
-    aff_lines.append('######## part 2 of 3 #########')
-    aff_lines.append('### Options for suggestion ###')
-    aff_lines.append('##############################')
-    aff_lines.append('')
-
-    aff_lines.append('##### key #####')
-    aff_lines.append('KEY qwertyuiop|asdfghjkl|zxcvbnm')
-    aff_lines.append('### end key ###')
-    aff_lines.append('')
-
-    #aff_lines.append('##### try #####')
-    #aff_lines.append('# Nothing looks ok at moment #')
-    #aff_lines.append('### end try ###')
-    #aff_lines.append('')
-
-    # What does maxngramsugs do in practice?
-    # If we set maxngramsugs to 0 then the word 'man' will
-    # get no suggestions, which is unacceptable.
-
-    # If we set this to 999 then the word 'manākitanga' gets these 9 suggestions
-    # 'manaakitanga', 'makitatanga', 'tamarikitanga', 'matanātanga', 
-    # 'mangaekatanga', 'mamangatanga', 'mangamangatanga', 'mairangatanga', 'kiritangata'
-    # which seems a bit over the top
-    # 
-    aff_lines.append('##### maxngramsugs #####')
-    aff_lines.append('# USE DEFAULT')
-    aff_lines.append('### end maxngramsugs ###')
-    aff_lines.append('')
-
-    # What does this rule do in practice?
-    # ANSWER HERE
-    aff_lines.append('##### nosplitsugs #####')
+    aff_lines.append('BREAK 0')
     aff_lines.append('NOSPLITSUGS')
-    aff_lines.append('### end nosplitsugs ###')
-    aff_lines.append('')
-
-    aff_lines.append('##### rep #####')
-    aff_lines.append('REP 20')
-    aff_lines.append('REP a ā')
-    aff_lines.append('REP ā a')
-    aff_lines.append('REP e ē')
-    aff_lines.append('REP ē e')
-    aff_lines.append('REP i ī')
-    aff_lines.append('REP ī i')
-    aff_lines.append('REP o ō')
-    aff_lines.append('REP ō o')
-    aff_lines.append('REP u ū')
-    aff_lines.append('REP ū u')
-    aff_lines.append('REP aa ā')
-    aff_lines.append('REP ā aa')
-    aff_lines.append('REP ee ē')
-    aff_lines.append('REP ē ee')
-    aff_lines.append('REP ii ī')
-    aff_lines.append('REP ī ii')
-    aff_lines.append('REP oo ō')
-    aff_lines.append('REP ō oo')
-    aff_lines.append('REP uu ū')
-    aff_lines.append('REP ū uu')
-    aff_lines.append('### end rep ###')
     aff_lines.append('')
 
     aff_lines.append('##### map #####')
@@ -156,24 +85,22 @@ def create_test_aff_file():
     aff_lines.append('### end map ###')
     aff_lines.append('')
 
-    # Part 3
-    aff_lines.append('################################')
-    aff_lines.append('######### part 3 of 3 ##########')
-    aff_lines.append('### Options for comppounding ###')
-    aff_lines.append('################################')
-    aff_lines.append('')
-
-    # What does this rule do in practice?
-    # This prevents foo-bar or bar-foo from being 'ok' if
-    # foo and bar are in the dictionary but not foo-bar or bar-foo
-    aff_lines.append('##### break #####')
-    aff_lines.append('BREAK 0')
-    aff_lines.append('### end break ###')
-    aff_lines.append('')
-
+    aff_lines.append('##### rep #####')
+    aff_lines.append('REP 10')
+    aff_lines.append('REP aa ā')
+    aff_lines.append('REP ā aa')
+    aff_lines.append('REP ee ē')
+    aff_lines.append('REP ē ee')
+    aff_lines.append('REP ii ī')
+    aff_lines.append('REP ī ii')
+    aff_lines.append('REP oo ō')
+    aff_lines.append('REP ō oo')
+    aff_lines.append('REP uu ū')
+    aff_lines.append('REP ū uu')
+    aff_lines.append('### end rep ###')
 
     # write the file
-    with open(test_dicaff_file_path, "a") as myfile:
+    with open(baseline_file_path, "a") as myfile:
         for aff_line in aff_lines:
             myfile.write(aff_line + "\n")
     return True
@@ -189,13 +116,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
-    # create the parser for the create_test_dic_file function
-    create_test_dic_file_parser = subparsers.add_parser('create_test_dic_file')
-    create_test_dic_file_parser.set_defaults(function = create_test_dic_file)
+    # create the parser for the create_hpk_dic_file function
+    create_hpk_dic_file_parser = subparsers.add_parser('create_hpk_dic_file')
+    create_hpk_dic_file_parser.set_defaults(function = create_hpk_dic_file)
 
-    # create the parser for the create_test_aff_file function
-    create_test_aff_file_parser = subparsers.add_parser('create_test_aff_file')
-    create_test_aff_file_parser.set_defaults(function = create_test_aff_file)
+    # create the parser for the create_baseline_aff_file function
+    create_baseline_aff_file_parser = subparsers.add_parser('create_baseline_aff_file')
+    create_baseline_aff_file_parser.set_defaults(function = create_baseline_aff_file)
 
     # parse the arguments
     arguments = parser.parse_args()
