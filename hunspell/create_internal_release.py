41e2966106e9304ca345cbd09a40d8daf056349d
graham-laptop <graham_oliver@yahoo.com>
2017-01-12 23:50:02 +0100

    bibi lappy

--- hunspell/create_internal_release.py
+++ hunspell/create_internal_release.py
@@ -78,32 +78,65 @@ def get_release_name(folder_or_path_name):
         return False
     else: 
         return release_name
+
+def get_release_number(folder_or_path_name):
+
+    '''
+    Given the folder name or the path name 
+    (It can be either because we are looking from the right)
+    return the release number (as integer)
+    It is returned as integer because we need to use it as a key
+    for sorting and key = int(get_release_number) didn't work in 
+    'sorted'
+    e.g 016_banana should return 16
+
+    Works up to 999
+    '''   
+    
+    try:
+        left_of_underscore = folder_or_path_name.rsplit("_", 1)[0]    
+    except IndexError:
+        print ("No underscore (_) found in the folder or path name")
+        return False
+
+    release_number = int(left_of_underscore[-3:])
+
+    return release_number    
     
 
-def get_internal_release_number(internal_release_name):
-    if internal_release_name == RELEASE_001_NAME:
-        return "001"
+def get_most_recent_internal_release():
+
+    '''
+    look in the internal releases files path at all the subfolders (if any)
+    find the most recent internal release (based on number)
+    return as string tuple    release number, release name
+    '''    
+
+    internal_releases_folders = os.listdir(internal_releases_files_path)
+    internal_releases_folders_paths = ([os.path.join(internal_releases_files_path, x) 
+                                        for x in internal_releases_folders])
+
+    if internal_releases_folders:
+        # check that there are only directories (no files)
+        assert all(os.path.isdir(x) for x in internal_releases_folders_paths)
+
+        # get the number of the most recent internal release
+        number_of_most_recent_internal_release = max([get_release_number(x) for x in \
+                                                 internal_releases_folders])
+
+        # get the name of the most recent internal release
+        most_recent_internal_release_folder = \
+            sorted(internal_releases_folders, key=get_release_number)[-1]
+
+        name_of_most_recent_internal_release = \
+            get_release_name(most_recent_internal_release_folder)
+
     else:
-        # look at the subfolders in the internal releases folder
-        # they are of the form "release_number"_"release_name"
-        # we need the highest release number and then add 1 to it
-        internal_releases_folders = os.listdir(internal_releases_files_path)
-        internal_releases_folders_paths = ([os.path.join(internal_releases_files_path, x) 
-                                            for x in internal_releases_folders])
+        # no internal releases as of yet
+        return None
 
-        if internal_releases_folders == []:
-            # the directory is empty
-            print("The first release can only be " + '"' + RELEASE_001_NAME + '"')
-            return False
-        else:
-            # check that there are only directories (no files)
-            assert all(os.path.isdir(x) for x in internal_releases_folders_paths)
-
-        # get the number of the last release (works up to 999)
-        number_of_last_release = max([int(x[:3]) for x in internal_releases_folders])
-        number_of_this_release = 1 + number_of_last_release
-
-        return str(number_of_this_release).zfill(3)       
+    return str(number_of_most_recent_internal_release).zfill(3), \
+           name_of_most_recent_internal_release    
 
 
 def get_supplemental_words(list_of_words):
@@ -161,65 +194,59 @@ def create_internal_release(internal_release_name=RELEASE_001_NAME):
     if not internal_release_number:
         return False
 
+    # get the .dic and .aff files we will be using
     if internal_release_number == "001":
-
-        # find those "open compound" or "mixed compound" words
-        # that contain 1 or more words (which could be hyphenated compounds)
-        # that are not in the .dic file
-        # known as "supplemental words"
-
-        BASELINE_DIC_FILE_NAME = "hpk.dic"
-        BASELINE_AFF_FILE_NAME = "baseline.aff"
-
-        dic_filepath = os.path.join(baseline_files_path, BASELINE_DIC_FILE_NAME)
-
-        dic_words = []
-
-        # read in the .dic file
-        with open(dic_filepath, "r") as f:
-            for line in f:
-                line_to_add = line.replace('\n', '')
-                try:
-                    # first line should contain an integer
-                    existing_dic_words_count = int(line_to_add) 
-                except:
-                    dic_words.append(line_to_add)
-        
-        # belt and braces check
-        assert existing_dic_words_count == len(dic_words)
-
-        # get the supplemental words
-        supplemental_words = get_supplemental_words(dic_words)
-
-        # get the open compounds
-        # there can be no open compounds in the supplemental words
-        # so only look in dic_words
-        open_compounds = []
-        for word in dic_words:
-            if " " in word and not "-" in word:
-                open_compounds.append(word)
-
-        # create the internal release directory
-        
-
-        # create and write the new internal release .dic file
-        new_dic_words_count = existing_dic_words_count + len(supplemental_words)
-        with open(new_dic_filepath, "a", encoding = 'utf-8') as myfile:
-            myfile.write(str(new_dic_words_count) + "\n")
-            for word in dic_words:
-                myfile.write(word + "\n")
-            for word in supplemental_words:
-                myfile.write(word + "\n")
-
-
-        # create and write the .aff file
-
-        return supplemental_words
-    
+        current_dic_filepath = os.path.join(baseline_files_path, "hpk.dic")
+        current_aff_filepath = os.path.join(baseline_files_path, "baseline.aff")
     else:
-        # look for .words file
         pass
+
+    dic_words = []
+
+    # read in the .dic file
+    with open(dic_filepath, "r") as f:
+        for line in f:
+            line_to_add = line.replace('\n', '')
+            try:
+                # first line should contain an integer
+                existing_dic_words_count = int(line_to_add) 
+            except:
+                dic_words.append(line_to_add)
     
+    # belt and braces check
+    assert existing_dic_words_count == len(dic_words)
+
+    # find those "open compound" or "mixed compound" words
+    # that contain 1 or more words (which could be hyphenated compounds)
+    # that are not in the .dic file - known as "supplemental words"
+    supplemental_words = get_supplemental_words(dic_words)
+
+    # get the open compounds
+    # there can be no open compounds in the supplemental words
+    # so only look in dic_words
+    open_compounds = []
+    for word in dic_words:
+        if " " in word and not "-" in word:
+            open_compounds.append(word)
+
+    # create the internal release directory
+    
+
+    # create and write the new internal release .dic file
+    new_dic_words_count = existing_dic_words_count + len(supplemental_words)
+    with open(new_dic_filepath, "a", encoding = 'utf-8') as myfile:
+        myfile.write(str(new_dic_words_count) + "\n")
+        for word in dic_words:
+            myfile.write(word + "\n")
+        for word in supplemental_words:
+            myfile.write(word + "\n")
+
+
+    # create and write the .aff file
+
+    return supplemental_words
+
+   
 
 if __name__ == '__main__':
 
@@ -243,10 +270,11 @@ if __name__ == '__main__':
     get_release_name_parser.add_argument('file_or_path_name')
     get_release_name_parser.set_defaults (function = get_release_name)
 
-    # create the parser for the get_internal_release_number function
-    get_internal_release_number_parser = subparsers.add_parser('get_internal_release_number')
-    get_internal_release_number_parser.add_argument('internal_release_name')
-    get_internal_release_number_parser.set_defaults(function = get_internal_release_number)
+    # create the parser for the get_most_recent_internal_release function
+    get_most_recent_internal_release_parser = \
+        subparsers.add_parser('get_most_recent_internal_release')
+    get_most_recent_internal_release_parser.set_defaults \
+        (function = get_most_recent_internal_release)
 
     # create the parser for the create_internal_release function
     create_internal_release_parser = subparsers.add_parser('create_internal_release')
